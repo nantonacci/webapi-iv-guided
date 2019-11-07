a95d910512570a7763ab77a3ac32f0ecfb089d4a
Nicole Antonacci <nicole.antonacci@gmail.com>
2019-11-06 19:51:48 -0800

    Test.

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const server = require('./api/server.js');
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+//const port = process.env.PORT;
+const { PORT } = process.env;
+
+server.listen(PORT, () => {
+  console.log(`\n*** Server Running on http://localhost:${PORT} ***\n`);
 });
+
+// const sortField = req.query.sortBy || 'id';
+//same thing=> const sortField = req.query.sortBy ? req.query.sortBy : 'id';
